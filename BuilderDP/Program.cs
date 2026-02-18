2513a88b8002e9066f985ebcb8955fcef2efc63a
amir <amirmoinirad@gmail.com>
2026-02-18 15:47:16 +0330

    Small Changes

--- BuilderDP/Program.cs
+++ BuilderDP/Program.cs
@@ -2,8 +2,8 @@
 // Amir Moeini Rad
 // September 2025
 
-// Main Concept: Builder Design Pattern
-// Help from Gemini
+// Main Concept: The Builder Design Pattern
+// With Help from Gemini
 
 // In this pattern, a complex object is constructed step by step and the final step will return the object.
 // The construction process can create different types and representations of an object using the same construction code.
@@ -34,31 +34,35 @@ namespace BuilderDP
     // Builder Interface
     public interface ICarBuilder
     {
-        void SetEngine(string engine);
-        void SetColor(string color);
-        void SetGPS(bool hasGPS);
+        SportsCarBuilder SetEngine(string engine);
+        SportsCarBuilder SetColor(string color);
+        SportsCarBuilder SetGPS(bool hasGPS);
         Car Build();
     }
 
     
     // Concrete Builder
+    // Modern .NET Style (Fluent Builder)
     public class SportsCarBuilder : ICarBuilder
     {
-        private Car _car = new Car();
+        private Car _car = new();
 
-        public void SetEngine(string engine)
+        public SportsCarBuilder SetEngine(string engine)
         {
             _car.Engine = engine;
+            return this;
         }
 
-        public void SetColor(string color)
+        public SportsCarBuilder SetColor(string color)
         {
             _car.Color = color;
+            return this;
         }
 
-        public void SetGPS(bool hasGPS)
+        public SportsCarBuilder SetGPS(bool hasGPS)
         {
             _car.HasGPS = hasGPS;
+            return this;
         }
 
         public Car Build()
@@ -76,26 +80,26 @@ namespace BuilderDP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Builder Design Pattern in C#.NET.");
-            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("The Builder Design Pattern in C#.NET.");
+            Console.WriteLine("-------------------------------------\n");
 
 
             var carBuilder = new SportsCarBuilder();
 
-            // Setting the properties step by step
+            // Setting the properties step by step (.NET Fluent Builder Style)
             // Properties of the car object are set using various methods of the builder.
             // This pattern is useful when the object has lots of optional parameters or when the construction process is complex.
             // Using this pattern, we don't need to set the propeties in the constructor of the class.
             // Otherwise, we would have to create multiple constructors for different combinations of parameters.
-            carBuilder.SetEngine("Electric");
-            carBuilder.SetColor("Blue");
-            carBuilder.SetGPS(false);
+            carBuilder.SetEngine("Electric")
+                      .SetColor("Blue")
+                      .SetGPS(false);
 
-            // Finally, we call the Build method to get the constructed object.
+            // Finally, we call the Build method to create the configured object.
             Car finalCar = carBuilder.Build();
 
-            // Displaying the built car specifications
+            // Displaying the built and configured car specifications
             finalCar.Display();
 
 
